30301a31f25aaccdb6d960328a672aa35d5b2e12
Rafaela Vitória Port <rafaela-vport@educar.rs.gov.br>
2023-03-15 13:33:36 -0300

    terceiro commit

--- js/script.js
+++ js/script.js
@@ -50,13 +50,42 @@ btnBuscarFilme.onclick = () => {
     return false;
 }
 
-btnBuscarFilme.onclick = () => {
+btnBuscarFilme.onclick = () =>{
     if(inputBuscarFilme.value.length > 0){
+        let filmes = new Array();
         fetch("http://www.omdbapi.com/?i=tt3896198&apikey=21ad4a64&s="+inputBuscarFilme.value, {mode:"cors"})
-        .then((resp)=> resp.json())
-        .then((resp)=> {
-            console.log(resp);
+        .then((resp)=>resp.json())
+        .then((resp)=>{
+            resp.Search.forEach((item)=>{
+                console.log(item);
+                let filme=new Filme(
+                    item.imdbID,
+                    item.Title,
+                    item.Year,
+                    null,
+                    null,
+                    item.Poster,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null
+                );
+                filmes.push(filme);
+            });
+            listarFilmes(filmes)
         })
     }
     return false;
+ }
+
+let listarFilmes = async (filmes)=>{
+    let listaFilmes = await document.querySelector("#lista-filmes");
+    listaFilmes.innerHTML = "";
+    console.log(listaFilmes);
+    if (filmes.length > 0){
+        filmes.forEach(async(filme)=>{
+            listaFilmes.appendChild(await filme. getCard());
+        });
+    }
 }
