8aa0399a749c46537b0fa41766a6fc215e71fef4
Rafaela Vitória Port <rafaela-vport@educar.rs.gov.br>
2023-04-06 15:27:21 -0300

    oitavo commit

--- js/classes.js
+++ js/classes.js
@@ -28,21 +28,29 @@ class Filme{
     getCard = () => {
         let card = document.createElement("div");
         card.setAttribute("class","card");
+
         let imgCartaz= document.createElement("img");
         imgCartaz.setAttribute("class", "card-img-topz");
         imgCartaz.setAttribute("src",this.cartaz);
+
         let cardBody = document.createElement("div");
         cardBody.setAttribute("class","card-body"); 
+
         let hCardTitle=document.createElement("h5");
         hCardTitle.setAttribute("class", "card-title"); 
+
         let divDetalhes = document.createElement("div");
-        divDetalhes.setAttribute("style","display:flex; justify-content:space-aroud;");
+        divDetalhes.setAttribute("style","display:flex;");
+
         let divGenero= document.createElement("div");
         divGenero.setAttribute("style", "flex-grow:1;"); 
+
         let divAnoProducao= document.createElement("div");
         divAnoProducao.setAttribute("style","flex-grow:1;");
+
         let divClassificacao= document.createElement("div");
         divClassificacao.setAttribute("style","flex-grow:1;"); 
+
         hCardTitle.appendChild(document.createTextNode(this.titulo)); 
         divGenero.appendChild(document.createTextNode(this.genero)); 
         divAnoProducao.appendChild(document.createTextNode(this.ano));
@@ -50,6 +58,7 @@ class Filme{
         divDetalhes.appendChild(divGenero);
         divDetalhes.appendChild(divAnoProducao);
         divDetalhes.appendChild(divClassificacao);
+
         card.appendChild(imgCartaz);
         card.appendChild(cardBody);
         cardBody.appendChild(hCardTitle);
@@ -58,16 +67,96 @@ class Filme{
         this.setBtnDetalhes();
         cardBody.appendChild(this.getBtnDetalhes());
 
-        return card;
+        return card
     }
-    setBtnDetalhes = () =>{
+    setBtnDetalhes= () => {
         this.btnDetalhes = document.createElement('button');
         this.btnDetalhes.appendChild(document.createTextNode("Detalhes"));
-        this.btnDetalhes.setAttribute("id", this.id);
-        this.btnDetalhes.setAttribute("class", "btnDetalhesFilme");
+        this.btnDetalhes.setAttribute("id",this.id);
+        this.btnDetalhes.setAttribute("class","btnDetalhesFilme");
     }
-    getBtnDetalhes = () =>{
+    getBtnDetalhes=()=>{
         return this.btnDetalhes
+    }   
+
+    getDetalhesFilmes=()=>{
+        let cardDetalhes = document.createElement("div");
+        cardDetalhes.setAttribute("class","cardDetalhes");
+
+        let imgCartaz= document.createElement("img");
+        imgCartaz.setAttribute("class", "img");
+        imgCartaz.setAttribute("src",this.cartaz);
+
+        let hCardTitle=document.createElement("h5");
+        hCardTitle.setAttribute("class", "card-title", "flex-grow:1");
+
+        let divDetalhes= document.createElement("div");
+        divDetalhes.setAttribute("style", "display:block;");
+
+        let divGenero= document. createElement("div");
+        divGenero.setAttribute ("style", "flex-grow:1;");
+
+        let divAnoProducao= document.createElement("div");
+        divAnoProducao.setAttribute("style", "flex-grow:1;");
+
+        let divClassificacao= document.createElement("div");
+        divClassificacao.setAttribute("style", "flex-grow:1;");
+
+        let divDuracao= document.createElement("div");
+        divDuracao.setAttribute("style", "flex-grow:1;");
+
+        let divSinopse= document.createElement("div");
+        divSinopse.setAttribute("style", "flex-grow:1;");
+
+        let divDirecao= document.createElement("div");
+        divDirecao.setAttribute("style", "flex-grow:1;");
+
+        let divElenco= document.createElement("div");
+        divElenco.setAttribute("style", "flex-grow:1;");
+
+        let divAvaliacao= document.createElement("div");
+        divAvaliacao.setAttribute("style", "flex-grow:1;");
+
+        let btnFechar = document.createElement("button");
+        btnFechar.appendChild(document.createTextNode("Fechar"));
+        btnFechar.setAttribute("id","btnFechar");
+
+        let btnSalvar = document.createElement("button");
+        btnSalvar.appendChild(document.createTextNode("Salvar"));
+        btnSalvar.setAttribute("id","btnSalvar")
+
+        hCardTitle.appendChild (document.createTextNode(this.titulo));
+        divGenero.appendChild(document.createTextNode(this.genero));
+        divAnoProducao.appendChild(document.createTextNode(this.ano));
+        divClassificacao.appendChild(document.createTextNode(this.classificacao));
+        divDuracao.appendChild(document.createTextNode(this.duracao));
+        divSinopse.appendChild(document.createTextNode(this.sinopse));
+        divDirecao.appendChild(document.createTextNode(this.direcao));
+        divElenco.appendChild(document.createTextNode(this.elenco));
+        divAvaliacao.appendChild(document.createTextNode(this.avaliacao));
+
+        
+        divDetalhes.appendChild(divSinopse);
+        divDetalhes.appendChild(divGenero);
+        divDetalhes.appendChild (divAnoProducao);
+        divDetalhes.appendChild(divClassificacao);
+        divDetalhes.appendChild(divDuracao);
+        divDetalhes.appendChild(divDirecao);
+        divDetalhes.appendChild(divElenco);
+        divDetalhes.appendChild(divAvaliacao);
+        divDetalhes.appendChild(btnFechar);
+        divDetalhes.appendChild(btnSalvar);
+
+        cardDetalhes.appendChild (imgCartaz);
+        cardDetalhes.appendChild (hCardTitle);
+        cardDetalhes.appendChild(divDetalhes);
+        cardDetalhes.append(imgCartaz);
+        cardDetalhes.append(hCardTitle);
+        cardDetalhes.append(divDetalhes);
+        
+        return cardDetalhes;
+
+        
     }
-    
+        
 }
--- js/script.js
+++ js/script.js
@@ -1,47 +1,8 @@
-let ator = new Ator(1, "John Wayne");
-console.log(ator);
-
-let diretor = new Diretor(1, "Alfred Hitchcock");
-console.log(diretor);
-
-let direcao =[
-    new Diretor(1, "Lana Wachowski"),
-    new Diretor(2, "Lilly Wachowski")
-];
-
-let elenco =[
-    new Ator(1, "Keanu Reeves"),
-    new Ator(2, "Carrie-Anne Moss"),
-    new Ator(3, "Laurence Fishburne"),
-    new Ator(4, "Joe Pantoliano"),
-    new Ator(5, "Hugo Weaving"),
-    new Ator(6, "Antony Ray Parker")
-];
-
-let genero = ["Ação","Aventura","Ficção cientifica"];
-
-
-let sinopse = "Um artista pobre e uma jovem rica se conhecem e se apaixonam na fatídica jornada do Titanic, em 1912. Embora esteja noiva do arrogante herdeiro de uma siderúrgica, a jovem desafia sua família e amigos em busca do verdadeiro amor.";
-
-let cartaz = "https://m.media-amazon.com/images/M/MV5BMDdmZGU3NDQtY2E5My00ZTliLWIzOTUtMTY4ZGI1YjdiNjk3XkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_SX300.jpg"
-
-let filme = new Filme(
-    1,
-    "Matrix",
-    1999,
-    genero,
-    102,
-    sinopse,
-    cartaz,
-    direcao,
-    elenco,
-    14,
-    null
-);
-console.log(filme);
-
 let inputBuscarFilme = document.querySelector("#input-buscar-filme");
 let btnBuscarFilme = document.querySelector("#btn-buscar-filme");
+let btnDetalhes = document.querySelector("#btn-detalhes");
+let btnSalvar = document.querySelector("#btn-salvar");
+let btn
 
 btnBuscarFilme.onclick = () => {
     if(inputBuscarFilme.ariaValueMax.length > 0){
@@ -79,28 +40,58 @@ btnBuscarFilme.onclick = () =>{
     return false;
  }
 
+
+
 let listarFilmes = async (filmes)=>{
-    let listaFilmes = await document.querySelector("#lista-filmes");
+    let listaFilmes = document.querySelector("#lista-filmes");
+    listaFilmes.style.display="flex";
     listaFilmes.innerHTML = "";
-    console.log(listaFilmes);
+    document.querySelector("#mostrar-filme").innerHTML="";
+    document.querySelector("#mostrar-filme").style.display="none";
+    //console.log(listaFilmes);
     if (filmes.length > 0){
         filmes.forEach(async(filme)=>{
             console.log(filme);
-            listaFilmes.appendChild( filme. getCard());
+            listaFilmes.appendChild( filme.getCard());
             filme.getBtnDetalhes().onclick=()=>{
                 detalhesFilme(filme.id);
             }
         });
     }
 }
-getBtnDetalhes=()=>{
-    return this.btnDetalhes;
-}
-let detalhesFilme = async (id)=>{
-    fetch("http://www.omdbapi.com/?apikey=21ad4a64&i="+id)
-    .then((resp)=> resp.json())
-    .then((resp)=> {
-        console.log(resp)
-    });
-    
-}
+            
+let detalhesFilme =  (id)=>{
+    fetch("https://www.omdbapi.com/?apikey=956c739e&i="+id)
+    .then ((resp)=> resp.json())
+    .then ((resp)=>{
+        console.log (resp); 
+        let filme=new Filme
+        ( resp.imdbID, 
+            resp.Title, 
+            resp.Year, 
+            resp.Genre.split(","), 
+            resp.Runtime, 
+            resp.Poster, 
+            resp.Plot, 
+            resp.Director, 
+            resp.Actors.split(","), 
+            resp.Awards, 
+            resp.imdbRating, 
+            ) 
+
+            console.log(filme)
+            document.querySelector ("#mostrar-filme").appendChild(filme.getDetalhesFilmes());
+
+            document.querySelector("#btnFechar").onclick = () => {
+                document.querySelector("#mostrar-filme").style.display="none";
+                document.querySelector("#mostrar-filme").innerHTML="";
+                document.querySelector("#lista-filmes").style.display="flex";
+            };
+
+            document.querySelector("#btnSalvar").onclick = () => {
+                salvarFilme(filme);
+            };
+            
+            document.querySelector("#lista-filmes").style.display="none";
+            document.querySelector("#mostrar-filme").style.display="block";
+    })}; 
